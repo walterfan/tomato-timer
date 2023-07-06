241dcd95bdfe903c9cfee9d4bbc850c1ee3942b4
Walter Fan <yafan@cisco.com>
2023-07-06 08:41:41 +0800

    update pop time to 5 minutes

--- tomato_timer.py
+++ tomato_timer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import time
-from tkinter import ttk
-from tkinter import Tk, StringVar, Entry, Label, Button
-from tkinter import messagebox
+from datetime import datetime, timezone
+from tkinter import Tk, StringVar, Entry, Label, Button, ttk, messagebox
 from playsound import playsound
 
 DEFAULT_MINS = 25
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 class TomatoTimer:
 
@@ -14,22 +14,18 @@ class TomatoTimer:
         self._root = Tk()
         self._style = ttk.Style()
         self._style.theme_use('alt')
-        # Define the geometry of the window
-        self._root.geometry(frameSize)
 
-        #define title
+        self._root.geometry(frameSize)
         self._root.title(title)
 
-        # set background color
-        #self._root.config(bg='green')
-
-        # declaration of variables
         self._hour=StringVar()
         self._minute=StringVar()
         self._second=StringVar()
 
         self.init_ui()
+
         self._started = False
+        self._tomato_count = 0
 
     def set_time(self, hour=0, min=DEFAULT_MINS, sec=0):
         self._hour.set("{0:2d}".format(hour))
@@ -45,7 +41,7 @@ class TomatoTimer:
         x_value = 90
         y_value = 80
         x_step = 60
-        # Using Entry class to take input from the user
+
         hour_box = self.create_time_box(self._hour)
         hour_box.place(x=x_value, y=y_value)
 
@@ -57,11 +53,10 @@ class TomatoTimer:
         sec_box = self.create_time_box(self._second)
         sec_box.place(x=x_value,y=y_value)
 
-        #Create a Label
         label = Label(self._root, text= "Focus on righ now",font=('Helvetica bold', 20))
-        #label.grid(column=0, row=0, padx=20, pady=20,  sticky='w')
+
         label.pack(pady=20)
-        # button widget
+
         y_value += 40
         btnStart = Button(self._root, text='Start', bd='5', fg="green", command= self.countdown)
         btnStart.place(x = 80,y = y_value)
@@ -74,13 +69,11 @@ class TomatoTimer:
 
     def stay_on_top(self):
         self._root.lift()
-        self._root.after(2000, self.stay_on_top)
+        self._root.after(300_000, self.stay_on_top)
 
     def run(self):
-        #Make the window jump above all
         self.stay_on_top()
         self._root.mainloop()
-
 
     def reset(self):
         self._started = False
@@ -89,32 +82,33 @@ class TomatoTimer:
     def pause(self):
         self._started = False
 
+    def get_hour_min_sec(self, left_seconds):
+
+        mins,secs = divmod(left_seconds, 60)
+
+        hours=0
+        if mins > 60:
+            hours, mins = divmod(mins, 60)
+        return hours, mins, secs
+
     def countdown(self):
         self._started = True
         try:
-            # store the user input
             left_seconds = int(self._hour.get())*3600 + int(self._minute.get())*60 + int(self._second.get())
         except:
-            messagebox.showwarning('', 'Invalid Input!')
+            messagebox.showwarning('', 'Invalid Input Value!')
+
         while self._started:
-            
-            # divmod(firstvalue = left_seconds//60, secondvalue = left_seconds%60)
-            mins,secs = divmod(left_seconds,60)
 
-            # Converting the input entered in mins or secs to hours,
-            hours=0
-            if mins > 60:
-                hours, mins = divmod(mins, 60)
-
+            hours, mins, secs = self.get_hour_min_sec(left_seconds)
             self.set_time(hours, mins, secs)
 
-            # updating the GUI window
             self._root.update()
             time.sleep(1)
 
             if (left_seconds == 0):
-                #messagebox.showinfo("Time Countdown", "Time Over")
-                print("It is time")
+                self._tomato_count += 1
+                print("It is time: {}, count={}".format( datetime.now().strftime(TIME_FORMAT),self._tomato_count))
                 playsound('starlet.wav', block = False)
                 break
 
